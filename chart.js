1febf2dbae9040092c3196584a952534ca439f49
Nick <141887142+Nick38213@users.noreply.github.com>
2026-04-05 15:07:29 -0300

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,21 @@
+new Chart(document.getElementById("grafico"), {
+  type: 'line',
+  data: {
+    labels: ["Jan","Fev","Mar"],
+    datasets: [{
+      label: "Carros",
+      data: [10, 15, 22]
+    }]
+  }
+});
+
+new Chart(document.getElementById("grafico"), {
+  type: 'line',
+  data: {
+    labels: ["Jan","Fev","Mar"],
+    datasets: [{
+      label: "Carros",
+      data: [10, 15, 22]
+    }]
+  }
+});
